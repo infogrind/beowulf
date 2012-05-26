40cc2a77c79c2c698f04e95f39140c065f92cef6
Marius Kleiner <infogrind@hotstuff.ch>
2012-05-26 23:36:08 +0200

    Test illustrating ws proxy factory differences

---
+++ beowulf-ws-testclient/src/test/java/ch/hotstuff/beowulf/wsclient/AlternativeProxyFactoryTest.java
@@ -0,0 +1,73 @@
+package ch.hotstuff.beowulf.wsclient;
+
+import static org.junit.Assert.assertEquals;
+
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.junit.Test;
+import org.springframework.remoting.jaxws.JaxWsPortProxyFactoryBean;
+
+import ch.hotstuff.beowulf.services.HelloWorld;
+
+
+public class AlternativeProxyFactoryTest
+{
+	@Test
+	public void cxfProxyFactoryBeanInstantiation()
+	{
+		System.out.println("Instantiating factoryBean");
+		final JaxWsProxyFactoryBean factoryBean = new JaxWsProxyFactoryBean();
+		
+		System.out.println("Setting properties");
+		factoryBean.setServiceClass(HelloWorld.class);
+		factoryBean.setAddress("http://localhost:8443/webservice/HelloWorld");
+		
+		System.out.println("Creating proxy");
+		HelloWorld service = (HelloWorld) factoryBean.create();
+		
+		System.out.println("Calling method");
+		assertEquals("Hello, world!", service.sayHello());
+		
+		System.out.println("Calling again");
+		assertEquals("Hello, world!", service.sayHello());
+		
+		System.out.println("Creating second proxy");
+		HelloWorld service2 = (HelloWorld) factoryBean.create();
+		
+		System.out.println("Calling method");
+		assertEquals("Hello, world!", service2.sayHello());
+		
+		System.out.println("Calling again");
+		assertEquals("Hello, world!", service2.sayHello());
+	}
+	
+	@Test
+	public void springProxyFactoryBeanInstantiation()
+	{
+		System.out.println("Instantiating factoryBean");
+		final JaxWsPortProxyFactoryBean factoryBean = new JaxWsPortProxyFactoryBean();
+		
+		System.out.println("Setting properties");
+		factoryBean.setServiceInterface(HelloWorld.class);
+		factoryBean.setEndpointAddress("http://localhost:8443/webservice/HelloWorld");
+		factoryBean.setServiceName("HelloWorld");
+		factoryBean.afterPropertiesSet();
+		
+		System.out.println("Creating proxy");
+		HelloWorld service = (HelloWorld) factoryBean.getObject();
+		
+		System.out.println("Calling method");
+		assertEquals("Hello, world!", service.sayHello());
+		
+		System.out.println("Calling again");
+		assertEquals("Hello, world!", service.sayHello());
+		
+		System.out.println("Creating second proxy");
+		HelloWorld service2 = (HelloWorld) factoryBean.getObject();
+		
+		System.out.println("Calling method");
+		assertEquals("Hello, world!", service2.sayHello());
+		
+		System.out.println("Calling again");
+		assertEquals("Hello, world!", service2.sayHello());
+	}
+}
