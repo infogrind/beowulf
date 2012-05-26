6143244b35745442883565dacb79375706d9897a
Marius Kleiner <infogrind@hotstuff.ch>
2012-05-26 18:10:28 +0200

    Integrated logging into servlet

--- beowulf-ws-testclient/src/main/java/ch/hotstuff/beowulf/wsclient/servlet/WsClientServlet.java
+++ beowulf-ws-testclient/src/main/java/ch/hotstuff/beowulf/wsclient/servlet/WsClientServlet.java
@@ -8,14 +8,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+
 public class WsClientServlet extends HttpServlet
 {
 	private static final long serialVersionUID = 1L;
+	
+	private Logger LOG = Logger.getLogger(this.getClass());
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 	throws ServletException, IOException 
 	{
+		LOG.info("Request received");
 		final PrintWriter out = response.getWriter();
 		response.addHeader("Content-type", "text/plain");
 		out.println("Hundwyler up and running");
