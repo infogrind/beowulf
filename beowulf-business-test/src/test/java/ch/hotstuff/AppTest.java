c2e4151b074d2ed328f4504ca0c5c212ecc5bba5
Marius Kleiner <infogrind@hotstuff.ch>
2012-11-19 23:19:27 +0100

    Test application context instantiates bean

--- beowulf-business-test/src/test/java/ch/hotstuff/AppTest.java
+++ beowulf-business-test/src/test/java/ch/hotstuff/AppTest.java
@@ -5,10 +5,13 @@ import junit.framework.TestCase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import ch.hotstuff.beowulf.services.HelloWorld;
 
 /**
  * Unit test for simple App.
@@ -18,10 +21,21 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class AppTest extends TestCase implements ApplicationContextAware
 {
 	private ApplicationContext appContext;
+
+	@Autowired
+	private HelloWorld helloBean;
+
 	@Test
 	public void testSpringContext()
 	{
 		assertNotNull(appContext);
+		assertNotNull(helloBean);
+	}
+
+	@Test
+	public void inverseOfStringIsCorrect()
+	{
+		assertEquals("renielk suiram", helloBean.revert("marius kleiner"));
 	}
 
 	@Override
