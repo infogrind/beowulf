ecd0bb7a286b2eddb4042ef18dcab9c38903b149
Marius Kleiner <infogrind@hotstuff.ch>
2012-11-24 20:42:12 +0100

    Re-enabled selenium test involving service call

--- beowulf-frontend-struts/src/test/java/ch/hotstuff/beowulf/frontend/struts/test/selenium/WelcomePageIT.java
+++ beowulf-frontend-struts/src/test/java/ch/hotstuff/beowulf/frontend/struts/test/selenium/WelcomePageIT.java
@@ -3,7 +3,6 @@ package ch.hotstuff.beowulf.frontend.struts.test.selenium;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
@@ -14,35 +13,34 @@ import ch.hotstuff.beowulf.frontend.struts.test.selenium.pages.WelcomePage;
 public class WelcomePageIT
 {
 	private static WebDriver driver;
-	
+
 	@Before
 	public void init()
 	{
 		driver = new HtmlUnitDriver();
 	}
-	
+
 	@Test
 	public void welcomePageHasCorrectTitle()
 	{
 		final String EXPECTED_TITLE = "Hello, World!";
 		WelcomePage page = WelcomePage.navigateTo(driver);
 		final String actualTitle = page.getTitleText();
-		
+
 		assertEquals(EXPECTED_TITLE, actualTitle);
 	}
-	
+
 	@Test
-	@Ignore
 	public void foodinfoPageShowsCorrectMessage()
 	{
 		final String FOOD = "Blutwurst";
 		final String DRINK = "Bier";
 		final String EXPECTED_MESSAGE = "Your favorite food is Blutwurst; your favorite drink is Bier";
-		
+
 		final WelcomePage welcomePage = WelcomePage.navigateTo(driver);
 		final FoodinfoPage infoPage = welcomePage.enterFoodInfo(driver, FOOD, DRINK);
 		final String infoMessage = infoPage.getInfoText();
-		
+
 		assertEquals(EXPECTED_MESSAGE, infoMessage);
 	}
 }
