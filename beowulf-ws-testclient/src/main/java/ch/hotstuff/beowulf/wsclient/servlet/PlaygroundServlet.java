87731ab2f91255b380cff1b8e8d5d124104c9b59
Marius Kleiner <infogrind@hotstuff.ch>
2013-07-21 00:43:17 +0200

    Added slf4j logging

--- beowulf-ws-testclient/src/main/java/ch/hotstuff/beowulf/wsclient/servlet/PlaygroundServlet.java
+++ beowulf-ws-testclient/src/main/java/ch/hotstuff/beowulf/wsclient/servlet/PlaygroundServlet.java
@@ -8,14 +8,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class PlaygroundServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
+
+	final private Logger LOG = LoggerFactory.getLogger(this.getClass());
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 	throws ServletException, IOException
 	{
+		LOG.info("Starting servlet for {}", this.getClass());
 		final PrintWriter out = response.getWriter();
 		response.setHeader("Content-type", "text/plain");
 
