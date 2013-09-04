3362bc72b9b0ec0f56eff5e8bebaac52d4d05c58
Marius Kleiner <infogrind@hotstuff.ch>
2013-09-04 22:41:32 +0200

    Included angularjs

--- beowulf-ajax/scripts/main.js
+++ beowulf-ajax/scripts/main.js
@@ -1,13 +1,21 @@
 console.log('main.js: Running');
 
 requirejs.config({
-	baseUrl: 'scripts'
+	baseUrl: 'scripts',
+	paths : {
+		angular : '../ext/scripts/angular'
+	},
+	shim : {
+		'angular' : { 'exports' : 'angular' }
+	}
 });
 
 var app = 'application';
 
 // Enable this only in dev mode
 app = 'applicationDev';
+
+require(['angular']);
 
 require([app], function(application) {
 	console.log("main.js: Pulling in application and initializing");
