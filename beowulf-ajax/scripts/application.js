c4a18332945d170025fce0340591b7e0df15da7f
Marius Kleiner <marius.kleiner@adnovum.ch>
2013-09-05 18:36:03 +0200

    Comments for application module and controllers.js

--- beowulf-ajax/scripts/application.js
+++ beowulf-ajax/scripts/application.js
@@ -1,6 +1,12 @@
 define(['util', 'angular', 'controllers'], function(util, angular) {
 	console.log("application.js: Starting up");
 	
+	/* Define the main application module. This will be passed to
+	 * angular.bootstrap() below, so that all services and all dependencies
+	 * of the application module are available.
+	 * The controllers are defined in a separate module myControllers;
+	 * see controllers.js.
+	 */
 	var appModule = angular.module('myApp', ['myControllers']);
 	
 	appModule.factory('myService', function () {
--- beowulf-ajax/scripts/controllers.js
+++ beowulf-ajax/scripts/controllers.js
@@ -1,4 +1,8 @@
 /*global define*/
+
+/* Each controller implementation is defined in a separate file in the controllers
+ * directory and loaded as a RequireJS dependency.
+ */
 define(['angular', 'controllers/myCtrl'], function (angular, myCtrl) {
 	var module = angular.module("myControllers", []);
 	module.controller('myController', myCtrl);
