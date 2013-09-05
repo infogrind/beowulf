b75b51593d5ecabb3d1081a4bc5ddecc3ede4cb0
Marius Kleiner <marius.kleiner@adnovum.ch>
2013-09-05 18:56:06 +0200

    Added comment about the usage of an injected service

--- beowulf-ajax/scripts/controllers/myCtrl.js
+++ beowulf-ajax/scripts/controllers/myCtrl.js
@@ -1,6 +1,9 @@
 /*global define*/
 define(['angular'], function() {
-	// This controller is pulled in via controllers.js.
+	/* This controller is pulled in via controllers.js.
+	 * Because the myControllers module defines a dependency on the myServices module,
+	 * the services of the latter (such as myService) are available for injection here.
+	 */
 	function myCtrl($scope, myService) {
 		$scope.serviceResult = myService.sayHello();
 	}
